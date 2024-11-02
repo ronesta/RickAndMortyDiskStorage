25a32f1d5808688d80e9078b54f99e8f6f1485a8
ronesta <gvozzzd123@gmail.com>
2024-11-02 17:25:52 +0300

    Initial commit

---
+++ RickAndMortyDiskStorage/RickAndMortyDiskStorage/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RickAndMortyDiskStorage
+//
+//  Created by Ибрагим Габибли on 02.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBrown
+    }
+}
+
